c30787c0832df5aff67bd5a27037228bde8e7331
Andrew Dinn <adinn@redhat.com>
2024-11-07 13:09:49 +0000

    generate x86 blob, stub and entry code from templates

--- src/hotspot/cpu/x86/stubGenerator_x86_64.hpp
+++ src/hotspot/cpu/x86/stubGenerator_x86_64.hpp
@@ -28,6 +28,7 @@
 #include "code/codeBlob.hpp"
 #include "runtime/continuation.hpp"
 #include "runtime/stubCodeGenerator.hpp"
+#include "runtime/stubRoutines.hpp"
 
 // Stub Code definitions
 
@@ -641,7 +642,7 @@ class StubGenerator: public StubCodeGenerator {
   void generate_final_stubs();
 
  public:
-  StubGenerator(CodeBuffer* code, StubsKind kind);
+  StubGenerator(CodeBuffer* code, StubGenBlobId blob_id);
 };
 
 #endif // CPU_X86_STUBGENERATOR_X86_64_HPP
